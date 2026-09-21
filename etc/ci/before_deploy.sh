8455e3cc19c9cee678ced06504208f42f5fb0b91
eugenesvk <eugenesvk@users.noreply.github.com>
2026-09-21 14:21:02 +0700

    __temp manually rename .sh before deploy

--- etc/ci/before_deploy.sh
+++ etc/ci/before_deploy.sh
@@ -11,7 +11,7 @@ pack() {
 
     tempdir=$(mktemp -d 2>/dev/null || mktemp -d -t tmp)
     out_dir=$(pwd)
-    package_name="$PROJECT_NAME-${GITHUB_REF/refs\/tags\//}-$TARGET"
+    package_name="$PROJECT_NAME-x-$TARGET"
 
     if [[ $TARGET == "arm-unknown-linux-gnueabihf" ]]; then
         gcc_prefix="arm-linux-gnueabihf-"
